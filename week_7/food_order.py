31ca5c1eb2a0b5b368a8e5aed6277ff07665443d
eiraahmed31-afk <eiraahmed31@gmail.com>
2026-07-08 15:47:51 +0000

    Week 7 tutorial

--- week_7/food_order.py
+++ week_7/food_order.py
@@ -1,1 +1,7 @@
+def calculate_total(price, quantity):
+    if price < 0:
+        raise ValueError("invalid price")
+    if quantity < 0:
+        raise ValueError("invalid quantity")
 
+    return price * quantity
--- week_7/test_food_order.py
+++ week_7/test_food_order.py
@@ -1,1 +1,23 @@
+from food_order import calculate_total
+import pytest
 
+
+def test_order1():
+    assert calculate_total(10, 2) == 20
+
+def test_order2():
+    assert calculate_total(10, 3) == 30
+
+def test_order3():
+    assert calculate_total(20, 5) == 100
+
+def test_order4():
+    assert calculate_total(5, 2) == 10
+
+def test_invalid_price():
+    with pytest.raises(ValueError):
+        calculate_total(-10, 2)
+
+def test_invalid_quantity():
+    with pytest.raises(ValueError):
+        calculate_total(10, -2)
